9b4579eb1080cc332395d6a099a59b2dc67a40a1
zianafique <44741072+zianafique@users.noreply.github.com>
2020-11-11 04:07:04 +0800

    submit

---
+++ flask.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 15 12:50:04 2020
+
+@modified by Zian Md Afique Amin
+@inspired By Zian
+"""
+
+
+from flask import Flask, request
+import pandas as pd
+import numpy as np
+import pickle
+import flasgger
+from flasgger import Swagger
+
+app=Flask(__name__)
+Swagger(app)
+
+
+pickle_in=open('classifier.pkl','rb')
+classifier=pickle.load(pickle_in)
+
+@app.route('/')
+def welcome():
+    return "Welcome All"
+
+@app.route('/predict')
+def predict_note_authentication():
+    
+    """Let's Authenticate the Banks Note 
+    This is using docstrings for specifications.
+    ---
+    parameters:  
+      - name: variance
+        in: query
+        type: number
+        required: true
+      - name: skewness
+        in: query
+        type: number
+        required: true
+      - name: curtosis
+        in: query
+        type: number
+        required: true
+      - name: entropy
+        in: query
+        type: number
+        required: true
+    responses:
+        200:
+            description: The output values
+        
+    """
+    variance=request.args.get("variance")
+    skewness=request.args.get("skewness")
+    curtosis=request.args.get("curtosis")
+    entropy=request.args.get("entropy")
+    prediction=classifier.predict([[variance,skewness,curtosis,entropy]])
+ 
+    return "The PRedicted Value is "+str(prediction)
+
+@app.route('/predict_file',methods=["POST"])
+def predict_note_file():
+    """Let's Authenticate the Banks Note 
+    This is using docstrings for specifications.
+    ---
+    parameters:
+      - name: file
+        in: formData
+        type: file
+        required: true
+      
+    responses:
+        200:
+            description: The output values
+        
+    """
+
+    
+    df_test=pd.read_csv(request.files.get("file"))
+    print(df_test.head())
+    prediction=classifier.predict(df_test)
+    
+    return str(list(prediction))
+
+
+
+
+
+
+
+if __name__=='__main__':
+    app.run()
+    
